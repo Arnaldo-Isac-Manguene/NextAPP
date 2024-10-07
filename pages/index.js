d2ca51e8db0f5f1b6c140eded7be8f2386fffe01
Arnaldo-Isac-Manguene <156318396+Arnaldo-Isac-Manguene@users.noreply.github.com>
2024-10-07 11:25:39 +0200

    coluna

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ function Home() {
                     </ul>
                 </div>
             </div>
-            <div class="col-5 ms-3  mt-5"  >
+            <div class="col-3 ms-3  mt-5"  >
                 <Formulario />
             </div>
         </div>
