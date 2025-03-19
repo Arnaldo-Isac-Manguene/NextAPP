e4238a08b87d49985f2b617c576730628c2651ac
Arnaldo-Isac-Manguene <156318396+Arnaldo-Isac-Manguene@users.noreply.github.com>
2025-03-19 16:24:30 +0200

    Update contacts.js

--- pages/contacts.js
+++ pages/contacts.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Contacts(){
     return <>
-            <h1>Nossos cotactos</h1>
+            <h1>Nossos contactos</h1>
             <p>Contactos da empresa ou instituição: </p>
                 voltar a página <Link href="/"> 
                     <a>Principal</a>
