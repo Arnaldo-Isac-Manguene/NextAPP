61e2e8f2bbb00a2be132847fab5c872a6cb4b3a2
Arnaldo-Isac-Manguene <156318396+Arnaldo-Isac-Manguene@users.noreply.github.com>
2026-03-18 08:27:40 +0200

    actualizacao do módulo de rodapé

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,12 @@ export default function Footer(){
     const year = new Date();
     return (
         <footer className={ styles.footer }>
+            <div className={ styles.informations }>
+                <Link href='/'>Home</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/contact'>Contact</Link>
+                <Link href='/product'>Product</Link>
+            </div>
             <p>Copyright &copy; { year.getUTCFullYear() } | TechModern</p>
         </footer>
     )
